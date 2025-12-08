22a260a4d4e4d465d7f5471daaf947aca8720206
Nausicaä Van West <root@khz.ac>
2025-12-08 00:28:56 -0500

    day 07, draft of part 2
    to do:
    - list_multiset/2 converts a list to an assoc of Pos - Count? maybe?
    - merge_multisets/3 merges two Pos - Count assoc
    
    then figure out how to use them lol
    
    Co-authored-by: Aspen Smith <root@gws.fyi>

--- day07.pl
+++ day07.pl
@@ -51,3 +51,50 @@ solution_part1(Solution) :-
     solution_part1(Diagram, Solution).
 
 %%%
+
+list_multiset(List, Multiset) :-
+    false.
+
+merge_multisets(Set1, Set2, Set) :-
+    false.
+
+beam_project_part2(Beams, Row, Next_beams) :-
+    aggregate_all(
+        bag(Y),
+        (
+            get_assoc(Y, _, Beams),
+            \+ nth0(Y, Row, '^')
+        ),
+        Unsplit_beams
+    ),
+
+    aggregate_all(
+        bag(Y),
+        (
+            (Y1 #= Y + 1 ; Y1 #= Y - 1),
+            will_be_split(Y1, Beams, Row)
+        ),
+        Split_beams
+    ),
+
+    append(Unsplit_beams, Split_beams, Next_beams).
+
+count_timelines([], Beams, Res) :-
+    length(Beams, Res).
+count_timelines([Row | Rows], Beams, Res) :-
+    beam_project_part2(Beams, Row, Next_beams),
+    count_timelines(Rows, Next_beams, Res).
+
+solution_part2([Source_row | Rows], Solution) :-
+    nth0(Y, Source_row, 'S'),
+    empty_assoc(Empty),
+    put_assoc(Y, Empty, 1, Beams),
+    count_timelines(Rows, Beams, Solution).
+
+solution_part2(Solution) :-
+    read_input("day07.input", Diagram),
+    solution_part2(Diagram, Solution).
+
+sample_part2(Solution) :-
+    read_input("day07.sample", Diagram),
+    solution_part2(Diagram, Solution).
